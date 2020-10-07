30ffe313addd8d5cf3d497b4221b8da7d9265d76
miguel <miguelangel.rendo@meisa-e.com>
2020-10-07 20:49:58 +0200

    set

---
+++ set.py
@@ -0,0 +1,12 @@
+paises = set (["España",
+    "Ecuador",
+    "Mexico",
+    "Peru",
+    "Peru",
+    "Colombia",
+    "Venezuela",])
+
+print(paises)
+sorted(paises)
+print(sorted(paises))
+print(sorted(paises, reverse=True))
